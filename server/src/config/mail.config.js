733f450786dbe2343553c1396d387acf9a0e3cab
GauravSharma902 <Digitalgauravflutter@gmail.com>
2026-07-25 19:37:04 +0530

    smtp issue resolved

--- server/src/config/mail.config.js
+++ server/src/config/mail.config.js
@@ -50,18 +50,39 @@ if (missingVariables.length > 0) {
 /**
  * Create SMTP Transporter
  */
+// const transporter = nodemailer.createTransport({
+//     // host: process.env.SMTP_HOST,
+//     // port: Number(process.env.SMTP_PORT) || 587,
+
+//     // secure: Number(process.env.SMTP_PORT) === 465,
+//     service: "gmail",
+//     auth: {
+//         user: process.env.SMTP_USER,
+//         pass: process.env.SMTP_PASS,
+//     },
+// });
+
 const transporter = nodemailer.createTransport({
-    // host: process.env.SMTP_HOST,
-    // port: Number(process.env.SMTP_PORT) || 587,
 
-    // secure: Number(process.env.SMTP_PORT) === 465,
-    service: "gmail",
+    host: process.env.SMTP_HOST,
+
+    port: Number(process.env.SMTP_PORT),
+
+    secure: false,
+
     auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
-});
 
+        user: process.env.SMTP_USER,
+
+        pass: process.env.SMTP_PASS,
+
+    },
+
+    tls: {
+        rejectUnauthorized: false
+    }
+
+});
 
 transporter.verify((error, success) => {
     if (error) {
